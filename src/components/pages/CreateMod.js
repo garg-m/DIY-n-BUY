e00888b41d843c6e3ae43b4bafea86a108f4693f
kireeti-r <87073865+kireeti-r@users.noreply.github.com>
2022-02-04 12:48:36 -0500

    "Updated Dashboard, login and register #1"

---
+++ src/components/pages/CreateMod.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateMod = () => {
+  return <>
+  <h1>Create Mod</h1>
+  <p>lorem 10
+      
+  </p>
+  </>;
+};
+
+export default CreateMod;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return <>
+  <h1> Home </h1>
+  <hr />
+  <p>Welcome to DIY-n-BUY!!</p>
+  <img src='https://media.istockphoto.com/photos/luxury-watch-isolated-on-white-background-with-clipping-path-for-or-picture-id1180244659?k=20&m=1180244659&s=612x612&w=0&h=kC3xPwi_AXqWqapmeVro6GhvQ2wcslD6WMgzdJbqZjE='alt='Watch Image1'/>
+  <img src='https://images.squarespace-cdn.com/content/v1/528552e5e4b0b26086d2c59c/1425648598813-FZUUCFHKLI88R67XA9OW/watch.jpg' alt='Watch Image 2'/>
+  
+  
+  </>;
+};
+
+export default Home;
