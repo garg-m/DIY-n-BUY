e4895c21e85892d029173c7fef9ee5e9135045c8
kireeti-r <87073865+kireeti-r@users.noreply.github.com>
2022-02-04 12:34:46 -0500

    "Updated Dashboard, user registration and login #1"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import Layout from "./components/pages/Layout";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./components/pages/Home";
+import CreateMod from "./components/pages/CreateMod";
+import LoginReg from "./components/pages/Auth/LoginReg";
+
+
+
 
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    
+      <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="CreateMod"  element={<CreateMod />} />
+          <Route path="LoginReg"  element={<LoginReg />} />
+          
+        </Route>
+        
+      </Routes>
+      </BrowserRouter>
+    </>
+    
+    
   );
 }
 
