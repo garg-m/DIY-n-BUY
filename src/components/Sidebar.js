7a69ceb43a448f5c0105a0f6dd65a38e64cca105
kireeti-r <87073865+kireeti-r@users.noreply.github.com>
2022-02-04 18:47:45 -0500

    "Resolved user authentication #1"

---
+++ src/components/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+const Nav=styled.div`
+background: #6d1b7b;
+height:80px;
+display:flex;
+align-items:center
+
+
+
+
+`;
+
+const Sidebar = () => {
+  return <Nav>
+      Nav
+  </Nav>;
+};
+
+export default Sidebar;
