2423e7ec0194ddca77910dc3f2a1af01e0271bcb
kireeti <rayaprolu.kireeti@gmail.com>
2022-02-21 17:36:12 -0500

    #17 Create ShopByParts Component which filters all parts in a table

---
+++ src/components/columnsShopByParts.js
@@ -0,0 +1,21 @@
+export const COLUMNS=[
+    {Header:'Id',
+     accessor:"id"},
+    {Header:'Cases',
+    accessor:"Cases"},
+    {Header:'Bezels',
+    accessor:"Bezels"},
+    {Header:'Bezel Inserts',
+    accessor:"Bezel Inserts"},
+    {Header:'Crowns',
+    accessor:"Crowns"},
+    {Header:'Dials',
+    accessor:"Dials"},
+    {Header:'Hands',
+    accessor:"Hands"},
+    {Header:'Chapter Rings',
+    accessor:"Chapter Rings"},
+    {Header:'Straps',
+    accessor:"Straps"}
+
+]
