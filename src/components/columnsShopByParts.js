99e42c03aed7d0ce44ec81eaa9fbcafcd597378d
kireeti <rayaprolu.kireeti@gmail.com>
2022-02-21 18:29:30 -0500

    #17 footer for table

--- src/components/columnsShopByParts.js
+++ src/components/columnsShopByParts.js
@@ -28,3 +28,36 @@ export const COLUMNS=[
     accessor:"Straps"}
 
 ]
+export const groupedColumns=[
+    {Header:'Id',
+Footer:'Id',
+accessor:'id'},
+    {Header:'Parts',
+Footer:'Parts',
+columns:[
+    {Header:'Cases',
+    Footer:"Cases",
+    accessor:"Cases"},
+    {Header:'Bezels',
+    Footer:"Bezels",
+    accessor:"Bezels"},
+    {Header:'Bezel Inserts',
+    Footer:"Bezel Inserts",
+    accessor:"Bezel Inserts"},
+    {Header:'Crowns',
+    Footer:"Crowns",
+    accessor:"Crowns"},
+    {Header:'Dials',
+    Footer:"Dials",
+    accessor:"Dials"},
+    {Header:'Hands',
+    Footer:"Hands",
+    accessor:"Hands"},
+    {Header:'Chapter Rings',
+    Footer:"Chapter Rings",
+    accessor:"Chapter Rings"},
+    {Header:'Straps',
+    Footer:"Straps",
+    accessor:"Straps"}
+]}
+]
