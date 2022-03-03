f7b0b6e4a9d26a604effe57d440384d6f216ac0d
garg-m <gargm1501@gmail.com>
2022-03-02 23:57:50 -0500

    #17 added all remaining tables for selecting parts

---
+++ src/components/pages/MenuPages/LimitedEdition.js
@@ -0,0 +1,134 @@
+import React, {useMemo} from "react";
+import { useTable, useSortBy, usePagination, useRowSelect } from "react-table";
+import MOCK_DATA from '../../MOCK_DATA.json';
+import {COLUMNS, groupedColumns} from '../../columnsShopByParts';
+import '../../tableShopByParts.css'
+import { style, width } from "@mui/system";
+import { Checkbox } from "@mui/material";
+export const LimitedEdition=()=>{
+    const columns=useMemo(()=> groupedColumns, [])
+    const data=useMemo(()=> MOCK_DATA, [])
+    const {tableInstance, selectedFlatRows} = useTable({
+        columns,
+        data,        
+    },
+    useRowSelect,
+        (hooks) => {
+            hooks.visibleColumns.push((columns) => {
+                return [
+                    {
+                        id: 'selection',
+                        Header: ({ getToggleAllRowsSelectedProps}) => (
+                            <Checkbox {...getToggleAllRowsSelectedProps()} />
+
+                        )
+                    },
+                    ...columns
+                ]
+            })
+        }, useSortBy, usePagination)
+    const{
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        
+        page,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage,
+        pageOptions,
+        gotoPage,
+        pageCount,
+        state,
+        prepareRow,
+    }=tableInstance 
+    const {pageIndex}=state
+    
+    return(
+        <>
+        <>
+        <table {...getTableProps()}>
+            <thead>
+                {
+                    headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {
+                                    headerGroup.headers.map(column => (
+                                        <th {... column.getHeaderProps(column.getSortByToggleProps)}>{column.render('Header')}
+                                        <span>
+                                            {column.isSorted? (column.isSortedDesc ? ' 🔽' : ' 🔼'):''}
+                                            </span>
+                                            </th>
+                                    ))
+                            }
+                    
+                </tr>
+                    ))
+
+                    
+                }
+                
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {
+                    page.map(row => {
+                        prepareRow(row)
+                        return(
+                            <tr {...row.getRowProps()}>
+                                {
+                                    row.cells.map(cell =>{
+                                       return <td{...cell.getCellProps()}>
+                                        {cell.render('Cell')}
+
+                                        </td>
+
+                                    })
+                                }
+                   
+                </tr>
+
+
+                        )
+                    })
+                }
+                
+            </tbody>
+            
+        </table>
+        <pre>
+                <code>
+                    {JSON.stringify(
+                        {
+                            selectedFlatRows: selectedFlatRows.map((row) => row.original),
+                        },
+                        null,
+                        2
+                    )}
+                </code>
+            </pre>
+        </>
+        <div align="center">
+            <span>
+                Page{' '}
+                <strong>
+                    {pageIndex+1} of {pageOptions.length}
+                </strong>{' '}
+            </span>
+            <span>
+                | Jump to: {' '}
+                <input type='number' defaultValue={pageIndex+1}
+                onChange={e=>{
+                    const pageNumber= e.target.value ? Number(e.target.value)-1 :0
+                    gotoPage(pageNumber)
+                    style={width:'50px'}
+                }}></input>
+            </span>
+            <button onClick={()=>gotoPage(0)}disabled ={!canPreviousPage}>{'<<'}</button>
+            <button onClick={()=>previousPage()} disabled={!canPreviousPage}>Previous</button>
+            <button onClick={()=>nextPage()} disabled={!canNextPage}>Next</button>
+            <button onClick={()=>gotoPage(pageCount-1)}disabled ={!canNextPage}>{'>>'}</button>
+        </div>
+        </>
+    )
+}
