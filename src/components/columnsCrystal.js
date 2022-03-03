fa70a0c21587283b67d99bedf16317f46e48e293
garg-m <gargm1501@gmail.com>
2022-03-02 23:57:50 -0500

    #17 added all remaining tables for selecting parts

---
+++ src/components/columnsCrystal.js
@@ -0,0 +1,41 @@
+export const columnsCrystal=[
+    {Header:'ID',
+    Footer:"ID",
+     accessor:"id"},
+    {Header:'Material',
+    Footer:"Material",
+    accessor:"Material"},
+    {Header:'DomeHeight',
+    Footer:"DomeHeight",
+    accessor:"DomeHeight"},
+    {Header:'Cyclops',
+    Footer:"Cyclops",
+     accessor:"Cyclops"},
+    {Header:'ImagePath',
+    Footer:"ImagePath",
+    accessor:"ImagePath"}
+
+
+]
+export const groupedColumnsCrystal=[
+    {Header:'Id',
+Footer:'Id',
+accessor:'id'},
+    {Header:'Details',
+Footer:'Details',
+columns:[
+    {Header:'Material',
+    Footer:"Material",
+    accessor:"Material"},
+    {Header:'DomeHeight',
+    Footer:"DomeHeight",
+    accessor:"DomeHeight"},
+    {Header:'Cyclops',
+    Footer:"Cyclops",
+     accessor:"Cyclops"},
+    {Header:'ImagePath',
+    Footer:"ImagePath",
+    accessor:"ImagePath"}
+    
+]}
+]
