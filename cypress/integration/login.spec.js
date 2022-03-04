62516c678ec93257f6d80ad33c92c835fe38efd1
kireeti <rayaprolu.kireeti@gmail.com>
2022-03-04 16:07:27 -0500

    Test for Login submission

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,13 @@
+
+
+describe("Select", () => {
+    beforeEach(()=>{
+        cy.visit('http://localhost:3000/LoginReg')
+    })
+    it("Should fill out id and password and submit",()=>{
+        cy.get('#email').type("rayaprolu.kireeti@gmail.com")
+        cy.get('#password').type("kireeti@128")
+        cy.get('#login-form').submit()
+
+    })
+})
