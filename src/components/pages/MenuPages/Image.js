bed705a27b188f684d9be5d7af2a9348b0c644ff
InjarapuSri <injarapusrisharanya@gmail.com>
2022-03-30 11:24:46 -0400

    Fetch image URL

---
+++ src/components/pages/MenuPages/Image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+
+  return(
+    <div>
+      <img src="https://drive.google.com/thumbnail?id='your photo link id'" alt="image" />
+    </div>
+  );
+}
+
+export default App;
