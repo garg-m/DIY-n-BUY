a66ebe7f189a5eeb2aae18daec389fc244b73dfb
garg-m <gargm1501@gmail.com>
2022-03-31 17:30:26 -0400

    Keep Original

--- src/components/partComponents/TableHand.js
+++ src/components/partComponents/TableHand.js
@@ -98,6 +98,9 @@ export const TableHand=()=>{
             </span>
             <button onClick={()=>gotoPage(0)}disabled ={!canPreviousPage}>{'<<'}</button>
             <button onClick={()=>previousPage()} disabled={!canPreviousPage}>Previous</button>
+            <Link to="/Cart">
+            <button onClick={()=>localStorage.getItem('row click Hand')} >Cart</button>
+            </Link>
             <button onClick={()=>nextPage()} disabled={!canNextPage}>Next</button>
             <button onClick={()=>gotoPage(pageCount-1)}disabled ={!canNextPage}>{'>>'}</button>
         </div>
