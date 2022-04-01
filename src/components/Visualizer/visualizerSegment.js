0ca27f22a4ec8c4f2fd9861ce085c92c27cff5f6
harsh-soni <harshsoni@outlook.com>
2022-04-01 16:33:13 -0400

    modified URL paths to Gdrive #8

--- src/components/Visualizer/visualizerSegment.js
+++ src/components/Visualizer/visualizerSegment.js
@@ -16,28 +16,28 @@ export const VisualizerSegment = () => {
 
         <div style={divStyle}>
             <div id="straps" style={imgStyle}>             
-            <img src="https://usa.crystaltimes.net/wp-content/themes/pro-child/Images/watch1/straps.png" alt="img"/>
+            <img src="https://drive.google.com/uc?id=1GGXEBN_zoNdBlOim49AYuTAgq7uGff2r" alt="img"/>
             </div>
             <div id="case" style={imgStyle}>
-                <img src="https://usa.crystaltimes.net/wp-content/uploads/2019/01/CT700-Polished-Front-2.png" alt="img"/>
+                <img src="https://drive.google.com/uc?id=13HCi4ZhEI3qlz8YysuzYgtYIiWruUD54" alt="img"/>
             </div>
             <div  id="dials" style={imgStyle}>
-                <img src="https://usa.crystaltimes.net/wp-content/uploads/2021/08/SKX007-Dial-1.png" alt="img"/>
+                <img src="https://drive.google.com/uc?id=1bXyjy5skNwPxMSXTqxqVAJRgbH3ziJbk" alt="img"/>
             </div>
             <div  id="hands" style={imgStyle}>
-                <img src="https://usa.crystaltimes.net/wp-content/uploads/2020/12/CT841-3.png" alt="img" />
+                <img src="https://drive.google.com/uc?id=15N23D5nXnA3mHAhngcWXw1Ztnjc5anbS" alt="img" />
             </div>
             <div id="crown" style={imgStyle}>
-                <img src="https://usa.crystaltimes.net/wp-content/uploads/2021/05/CT208-Polished-1.png" alt="img" />
+                <img src="https://drive.google.com/uc?id=1MvLUJsExLF18D5HzPZH9Hh3vEkCgz5m4" alt="img" />
             </div>
             <div  id="bezel" style={imgStyle}>
-                <img src="https://usa.crystaltimes.net/wp-content/uploads/2017/02/CT202CE-Polished-4.png" alt="img" />
+                <img src="https://drive.google.com/uc?id=1EXoFqI-0zGLMPhcqr8sNVcDaFSRg9XSQ" alt="img" />
             </div>
             <div  id="bezel-inserts" style={imgStyle}>
-                <img src="https://usa.crystaltimes.net/wp-content/uploads/2020/05/CT-680-Bezel-Insert-2.png" alt="img" />
+                <img src="https://drive.google.com/uc?id=1lTBuDdzufhVyo7i2N0oQlx19g1g9XN3v" alt="img" />
             </div>
             <div id="chapter-rings" style={imgStyle}>
-                <img src="https://usa.crystaltimes.net/wp-content/uploads/2021/08/SKX007-Chapter-Ring-3.png" alt="img"/>
+                <img src="https://drive.google.com/uc?id=1KrPGdoYzKrDcSpNRmpfaeKPNTmajD-tv" alt="img"/>
             </div>
         </div>
     )
