1403f625ef8570f8b14df3f263f9b772997a0445
InjarapuSri <injarapusrisharanya@gmail.com>
2022-03-31 22:31:52 -0400

    Fetch image URL

--- src/components/partComponents/TableCrystal.js
+++ src/components/partComponents/TableCrystal.js
@@ -68,7 +68,7 @@ export const TableCrystal=()=>{
                                         
                                         return(
                                             <div>
-                                              <img src="https://drive.google.com/thumbnail?id=13UjJskzgOjdIAPrQh4vitW-mQPZOe-oZ" alt="image" />
+                                              <img src="https://drive.google.com/uc?export=view&id=13UjJskzgOjdIAPrQh4vitW-mQPZOe-oZ" alt="image" />
                                             </div>
                                           );
 
