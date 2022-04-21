e4b8af06b1bd67924408a4bcf48ea942789dc064
garg-m <gargm1501@gmail.com>
2022-04-20 23:51:30 -0400

    Created Chapter Ring Table for admin

--- src/components/Inventory/Inventory Tables/TableChapterRingInventory.js
+++ src/components/Inventory/Inventory Tables/TableChapterRingInventory.js
@@ -11,3 +11,48 @@ import reactDom from "react-dom";
 //import { VisualizerSegment } from "../Visualizer/VisualizerSegment";
 import "./TableInventory.css"
 import {Grid, Card, Typography, Tabs, Tab, Box} from '@mui/material'
+
+const [tableData, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/getAll/chapterRing/', {
+            method: "GET",
+            mode: 'cors',
+        })
+            .then((response) => response.text()
+                .then(jsonContents => {
+                    console.log(JSON.parse(jsonContents))
+                    setData(JSON.parse(jsonContents))
+                })
+                .catch((error) => {
+                    console.error(error);
+                }));
+
+    }, []);
+
+    const columns = useMemo(() => groupedColumnsChapterRing, [])
+    const data = useMemo(() => MOCK_DATA_CASE, [])
+
+    const tableInstance = useTable({
+        columns,
+        data
+    }, useSortBy, usePagination, useRowSelect)
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        page,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage,
+        pageOptions,
+        gotoPage,
+        pageCount,
+        state,
+        prepareRow,
+    } = tableInstance
+
+    const { pageIndex } = state
+    const CircularJSON = require('circular-json')
