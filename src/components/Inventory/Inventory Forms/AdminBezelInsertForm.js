b51b669f106fe564864a5b446717c0cc0285c1b0
Srisharanya Injarapu <65551957+InjarapuSri@users.noreply.github.com>
2022-04-20 23:06:50 -0400

    Add AdminBezelInsertForm

---
+++ src/components/Inventory/Inventory Forms/AdminBezelInsertForm.js
@@ -0,0 +1,154 @@
+import React from 'react'
+import { useState } from 'react'
+
+const AdminBezelInsertForm = () => {
+
+    const [innerdiameter, setInnerdiameter]=useState(0)
+    const [outerdiameter, setOuterdiameter]=useState(0)
+    
+    const[style,setStyle]=useState('')
+    const[material,setMaterial]=useState('')
+    const[finish,setFinish]=useState('')
+    const[movements,setMovements]=useState('')
+    const[color,setColor]=useState('')
+    const[imagepath,setImagepath]=useState('')
+    
+    
+    const[type, setType]=useState('')
+    const handleSubmit=(e)=>{
+            e.preventDefault()
+            //this.props.history.push('/admin/inventory/successpage')
+            const BezelInsert={innerdiameter,outerdiameter,type,movements,material,finish,color,style,imagepath}
+            console.log(BezelInsert)
+            fetch('http://localhost:3001/create/bezelRings',{
+                method:'POST',
+                mode:'cors',
+                headers: {"Content-Type":"application/json charset=UTF-8"},
+                body: JSON.stringify(BezelInsert)
+            }).then((response) => {
+                if(response){
+                    window.location.href = "/admin/inventory/successpage";
+                }
+            })
+    }
+  return (
+      <div>
+          <h2 align="center">Welcome Admin!!!</h2>
+          <h3 align="center">Fill the form to add a new bezelinserts!!!</h3>
+          <form align="center" onSubmit={handleSubmit}>
+              <div >
+              <label>Type</label>
+        <input
+        type="text"
+        required
+        value={type}
+        onChange={(e)=> setType(e.target.value)}
+        />
+              </div>
+              <div>
+              <label>Movements</label>
+        <input
+        type="text"
+        required
+        value={movements}
+        onChange={(e)=>setMovements(e.target.value)}
+       
+        />
+              </div>
+        <div>
+        <label>Material</label>
+        <input
+        type="text"
+        required
+        value={material}
+        onChange={(e)=>setMaterial(e.target.value)}
+       
+        />
+        </div>
+       
+        <div>
+        <label>Finish</label>
+        <input
+        type="text"
+        required
+        value={finish}
+        onChange={(e)=>setFinish(e.target.value)}
+       
+        />
+        </div>
+        <div>
+        <label>Color</label>
+        <input
+        type="text"
+        required
+        value={color}
+        onChange={(e)=>setColor(e.target.value)}
+       
+        /> 
+
+</div>
+       
+       <div>
+       <label>Innerdiameter</label>
+       <input
+       type="text"
+       required
+       value={innerdiameter}
+       onChange={(e)=>setInnerdiameter(e.target.value)}
+      
+       />
+
+</div>
+       
+       <div>
+       <label>Outerdiameter</label>
+       <input
+       type="text"
+       required
+       value={outerdiameter}
+       onChange={(e)=>setOuterdiameter(e.target.value)}
+      
+       />
+
+</div>
+       
+       
+        <div>
+       <label>Style</label>
+       <input
+       type="text"
+       required
+       value={style}
+       onChange={(e)=>setStyle(e.target.value)}
+      
+       />
+        </div>
+
+        <div>
+       <label>Imagepath</label>
+       <input
+       type="text"
+       required
+       value={imagepath}
+       onChange={(e)=>setImagepath(e.target.value)}
+      
+       />
+        </div>
+        
+       
+        <div>
+        <button type='submit'>Add Part</button>
+        </div>
+        
+        
+
+
+    </form>
+    
+ 
+      </div>
+    
+  )
+}
+
+export default AdminBezelInsertForm
